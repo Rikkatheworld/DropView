a5c41f6a999ffcf91958580566c80fcc0dfb91b8
rikka Tsu <rikkatheworld.xie@narwel.com>
2019-04-26 15:13:17 +0800

    111

--- app/src/main/java/com/example/dropview/MainActivity.java
+++ app/src/main/java/com/example/dropview/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//wowowowowo
         final DropView drop = findViewById(R.id.dropview);
         drop.start();
         ValueAnimator valueAnimator = ValueAnimator.ofInt(0, 100);
